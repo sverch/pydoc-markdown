3408b62e7e6b3eb009d8eb692bcf5ffc15017ca7
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-05-03 20:00:50 +0200

    add new pydoc-markdown.conf.py file in docs/

---
+++ docs/pydoc-markdown.conf.py
@@ -0,0 +1,9 @@
+
+modules = [
+  'pydoc_markdown.core.base++',
+  'pydoc_markdown.core.document++',
+  'pydoc_markdown.core.python++',
+  'pydoc_markdown.utils.decorators++',
+  'pydoc_markdown.utils.imputils++',
+  'pydoc_markdown.utils.pydoc++',
+]
