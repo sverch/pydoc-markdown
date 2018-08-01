eb7cbd7a4102f9b39a393075b81a86b4b558df29
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-08-01 13:43:37 +0200

    remove docs/ folder, move pydoc_markdown/ into src/ subdirectory, move testmodule/__init__.py to test/docexample.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-# Copyright (C) 2016  Niklas Rosenstein
+# -*- coding: utf8 -*-
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Niklas Rosenstein
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in
@@ -14,9 +17,9 @@
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 
 import io
 import setuptools
@@ -28,16 +31,16 @@ with io.open('requirements.txt') as fp:
   requirements = fp.readlines()
 
 setuptools.setup(
-  name='pydoc-markdown',
-  version='3.0.0-dev',
-  description='Create Python API documentation in Markdown format',
-  long_description=readme,
-  long_description_content_type='text/markdown',
-  url='https://github.com/NiklasRosenstein/pydoc-markdown',
-  author='Niklas Rosenstein',
-  author_email='rosensteinniklas@gmail.com',
-  license='MIT',
-  classifiers=[
+  name = 'pydoc-markdown',
+  version = '3.0.0-dev',
+  description = 'Create Python API documentation in Markdown format',
+  long_description = readme,
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/NiklasRosenstein/pydoc-markdown',
+  author = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  license = 'MIT',
+  classifiers = [
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Intended Audience :: End Users/Desktop',
@@ -51,11 +54,12 @@ setuptools.setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
   ],
-  keywords='markdown pydoc generator docs documentation',
-  packages=setuptools.find_packages(),
-  install_requires=requirements,
-  entry_points=dict(
-    console_scripts=[
+  keywords = 'markdown pydoc generator docs documentation',
+  packages = setuptools.find_packages('src'),
+  package_dir = {'': 'src'},
+  install_requires = requirements,
+  entry_points = dict(
+    console_scripts = [
       'pydoc-markdown=pydoc_markdown.__main__:main',
     ]
   ),
